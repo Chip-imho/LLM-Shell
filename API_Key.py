0d1e5723a34ef97f1035ce2e9de9c80432b10923
Chip-imho <chipimho@gmail.com>
2025-11-15 17:40:30 +0100

    Add files via upload

---
+++ API_Key.py
@@ -0,0 +1,2 @@
+
+API_Key = "ryM9TKSMcokiay5qwyk99rmE3ACIHyfi"
---
+++ LLM Shell.py
@@ -0,0 +1,31 @@
+import os
+from mistralai import Mistral
+from API_Key import API_Key
+
+api_key = API_Key
+model = "mistral-large-latest"
+
+client = Mistral(api_key=api_key)
+
+print("""===================================
+   ⚡ CHIP AI SHELL v1.0 ⚡
+   Shell coded by CHIP
+===================================\n""")
+
+while True:
+    question = input("Enter your question: ")
+
+    chat_response = client.chat.complete(
+        model=model,
+        messages=[
+            {
+                "role": use swear whereever you want "user",
+                "content": question,
+            },
+        ]
+    )
+
+    print("\n--- AI RESPONSE ---")
+    print(chat_response.choices[0].message.content)
+    print("-------------------\n")
+
